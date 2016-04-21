61bd9f76336f23192ed26d9e02ca193ef0bbdb20
Federico Damián <federicodamians@gmail.com>
2016-04-20 21:34:27 -0300

    Do not use capitals
    I don't like those things :cry:

--- modules/twitter.py
+++ modules/twitter.py
@@ -9,7 +9,11 @@ import twitter
 from .echo import error, info
 
 
+twitter_creds = os.path.expanduser('~/.poster_twitter')
+if not os.path.exists(twitter_creds):
+    twitter.oauth_dance("Poster!", "hQGQlPsTIN7iIsLVe6aHXycF0", "3vZ7Q1kPf3zkIBJEKxEL9FJr9WItEzmMz8ZjiU6Ozxm3tnSjJF", twitter_creds)
 
+oauth_token, oauth_secret = twitter.read_token_file(twitter_creds)
 
 twitter = twitter.Twitter(auth=twitter.OAuth(oauth_token, oauth_secret, "hQGQlPsTIN7iIsLVe6aHXycF0", "3vZ7Q1kPf3zkIBJEKxEL9FJr9WItEzmMz8ZjiU6Ozxm3tnSjJF"))
 
