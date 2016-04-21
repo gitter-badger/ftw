0608544cdd99829d60fade300d9cbfb74e93a7df
Federico Damián <federicodamians@gmail.com>
2016-04-20 21:41:27 -0300

    Fix -ts nonsense.
    Because we are sending TWeets, no TSees

--- poster.py
+++ poster.py
@@ -8,8 +8,8 @@ import argparse
 from modules import twitter
 
 parser = argparse.ArgumentParser(description="Send a message, anywhere!")
-parser.add_argument("--twitter-status", "-ts", type=str, help="Send a tweet")
+parser.add_argument("--tweet", "-tw", type=str, help="Send a tweet")
 args = parser.parse_args()
 
-if args.twitter_status:
-    twitter.send(args.twitter_status)
+if args.tweet:
+    twitter.send(args.tweet)
